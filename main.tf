5c676725cabdef626c787169b79f4af8d2e54ebb
Oleg Ananyev <oleg@ananyev.me>
2021-10-23 16:36:02 +0300

    commit

--- main.tf
+++ main.tf
@@ -30,14 +30,28 @@ resource "aws_security_group" "forwarder" {
   }
 }
 
-#resource "aws_key_pair" "key" {
-#  public_key = "${var.public_key}"
-#}
+module "vpc" "diplom_vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "diplom_vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["eu-north-1a", "eu-north-1b", "eu-north-1c"]
+  #private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = false
+
+  tags = {
+    Terraform = "true"
+    Environment = "stage"
+  }
+}
 
 resource "aws_instance" "instance" {
   count = 3
-  subnet_id     = "subnet-04393788dce09388a"
+  subnet_id     = vpc.diplom_vpc.public_subnets[0] #"subnet-04393788dce09388a"
   ami           = data.aws_ami.ubuntu.id
-  #key_name      = "${aws_key_pair.key.key_name}"
   instance_type = "t3.micro"
 }
