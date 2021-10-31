57c5cb2e3719f1cc23e34c3f28774632c6d5822c
Oleg Ananyev <oleg@ananyev.me>
2021-10-31 20:57:43 +0300

    new aws account

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = "eu-north-1"
+  region = "us-east-2"
 }
 
 data "aws_ami" "ubuntu" {
@@ -69,7 +69,7 @@ resource "aws_instance" "diplom_instance" {
   count = 3
   subnet_id     = module.vpc.public_subnets[0]
   ami           = data.aws_ami.ubuntu.id
-  instance_type = "t3.micro"
+  instance_type = "t2.micro"
   vpc_security_group_ids = [aws_security_group.diplom_sec_group.id]
   key_name= "diplom-key-pair"
 }
