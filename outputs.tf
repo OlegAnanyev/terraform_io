d92697f81ed6ac7d216524d98a275c5e88c22730
Oleg Ananyev <32748936+OlegAnanyev@users.noreply.github.com>
2021-11-01 20:06:03 +0300

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -6,26 +6,3 @@ output "private_ip" {
 output "public_ip" {
  value = aws_instance.diplom_instance.*.public_ip
 }
-
-# //id аккаунта
-# output "account_id" {
-#   value = data.aws_caller_identity.current.account_id
-# }
-# //id пользователя
-# output "user_id" {
-#   value = data.aws_caller_identity.current.user_id
-# }
-# //регион
-# output "region" {
-#   value = data.aws_region.current.name
-# }
-
-# //ARN (Amazon Resource Name)
-# output "caller_arn" {
-#   value = data.aws_caller_identity.current.arn
-# }
-
-# //id подсети
-# output "subnet_id" {
-#   value = aws_instance.netology.*.subnet_id
-# }
